81c2eea59a8313bf184c659a54975fcab1f778ef
simulan <simulan88@yahoo.com>
2017-07-15 01:19:18 +0200

    Refactor Tests

--- src/UMLCreatorLibrary/Models/Decoders/MethodSerializer.cs
+++ src/UMLCreatorLibrary/Models/Decoders/MethodSerializer.cs
@@ -13,19 +13,21 @@ namespace UMLCreatorLibrary.Models.Decoders {
         public MethodSerializer(TypeAdapterChain chain) {
             parent = chain;
         }
+
         public Method Deserialize(String input) {
             this.input = input;
             int typeDelimiterIndex = FindLastIndexOrThrow(Internals.TYPE_DELIMITER);
             int parameterStartDelimiterIndex = FindIndexOrThrow(Internals.PARAMETER_START_DELIMITER);
             int parameterEndDelimiterIndex = FindIndexOrThrow(Internals.PARAMETER_END_DELIMITER);
             ValidateDelimiterOrder(typeDelimiterIndex, parameterStartDelimiterIndex, parameterEndDelimiterIndex);
-            //Might use Substring here
+
             return new Method(
-                DecodeAccessModifier(),
-                DecodeName(parameterStartDelimiterIndex),
-                DecodeParameters(parameterStartDelimiterIndex, parameterEndDelimiterIndex),
-                DecodeReturnType(typeDelimiterIndex));
+                DeserializeAccessModifier(),
+                DeserializeName(parameterStartDelimiterIndex),
+                DeserializeParameters(parameterStartDelimiterIndex, parameterEndDelimiterIndex),
+                DeserializeReturnType(typeDelimiterIndex));
         }
+
         private void ValidateDelimiterOrder(int typeDelimiterIndex, int parameterStartDelimiterIndex, int parameterEndDelimiterIndex) {
             if (typeDelimiterIndex < parameterEndDelimiterIndex) {
                 throw new FormatException("Must have a return type defined after last parenthesis");
@@ -56,7 +58,7 @@ namespace UMLCreatorLibrary.Models.Decoders {
             return indexOf;
         }
 
-        private AccessScope DecodeAccessModifier() {
+        private AccessScope DeserializeAccessModifier() {
             switch (input[0]) {
                 case Internals.ACCESS_PUBLIC: return AccessScope.PUBLIC;
                 case Internals.ACCESS_PROTECTED: return AccessScope.PUBLIC;
@@ -64,29 +66,26 @@ namespace UMLCreatorLibrary.Models.Decoders {
                 default: throw new FormatException("There is no access modifier such as '" + input[0] + "'");
             }
         }
-        private String DecodeName(int indexFirstParenthesis) {
+        private String DeserializeName(int indexFirstParenthesis) {
             const int PREFIX_OFFSET = 1;
             int argsLength = input.Length - indexFirstParenthesis;
             string name = input.Substring(PREFIX_OFFSET, input.Length - argsLength - PREFIX_OFFSET).Replace(" ", "");
-            if (name.Length == 0) throw new FormatException("Method has no name");
-            if (!Internals.IsAlpha(name)) throw new FormatException("Method has invalid characters");
+            Internals.ValidateString(name, "Method should have a valid name");
             return name;
         }
-
-        private List<Argument> DecodeParameters(int indexFirstParenthesis, int indexLastParenthesis) {
+        private List<Argument> DeserializeParameters(int indexFirstParenthesis, int indexLastParenthesis) {
             const int SINGLE_OFFSET = 1;
-            List<Argument> decodedParameters = new List<Argument>();
+            List<Argument> deserializedParameters = new List<Argument>();
             String argumentsInput = input.Substring(indexFirstParenthesis + SINGLE_OFFSET, indexLastParenthesis - indexFirstParenthesis - SINGLE_OFFSET);
             TypeAdapter<Argument> argumentDeserializer = parent.provide<Argument>() as TypeAdapter<Argument>;
             foreach (String part in argumentsInput.Split(Internals.VARIABLES_DELIMITER)) {
                 if (!part.Trim().Equals("")) {
-                    decodedParameters.Add(argumentDeserializer.Deserialize(part));
+                    deserializedParameters.Add(argumentDeserializer.Deserialize(part));
                 }
             }
-            return decodedParameters;
+            return deserializedParameters;
         }
-
-        private string DecodeReturnType(int indexColon) {
+        private string DeserializeReturnType(int indexColon) {
             string returnType = input.Substring(indexColon + 1).Replace(" ", "");
             Internals.ValidateString(returnType,"Valid return-type expected.");
             return returnType;
---
+++ src/UMLcreatorTestProject/UMLTest.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMLCreatorLibrary.Models;
+using UMLCreatorLibrary.Models.Decoders;
+
+namespace UMLcreatorTestProject {
+    [TestClass]
+    class UMLTest {
+        [TestMethod]
+        public void deserializeShouldThrowOnNoAccessModifier() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("a(var:type):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnNoName() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("+(var:type):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnNoParameterName() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("+(:type):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnNoParameterType() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("+(var:):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void deserializeShouldThrowOnInvalidAccessModifier() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("func(var:type):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnInvalidName() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("++(var:type):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnInvalidParameterName() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("+abc(+:type):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception e) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnInvalidParameterType() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("+abc(var:??):void");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void deserializeShouldThrowOnInvalidReturnType() {
+            UML uml = new UML();
+            try {
+                uml.Deserialize<Method>("+abc(var:type):€€");
+                Assert.Inconclusive("Should've thrown exception");
+            } catch (AssertInconclusiveException exception) {
+                Assert.Fail(exception.Message);
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+
+    }
+}
