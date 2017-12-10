cbd7f09895f98be869add2a5572a8d007012968b
simulan <simulan88@yahoo.com>
2017-12-10 20:17:03 +0100

    added Load to ImageLoader

---
+++ UMLProgram/Core/Loaders/ImageLoader.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLProgram.Core.Loaders {
+    public class ImageLoader {
+        public static int Load(string filename) {
+            try {
+                Bitmap file = new Bitmap(filename);
+                return Load(file);
+            } catch (FileNotFoundException e) {
+                return -1;
+            }
+        }
+        public static int Load(Bitmap image) {
+            int texID = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, texID);
+            Rectangle size = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData data = image.LockBits(size, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            image.UnlockBits(data);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+            return texID;
+        }
+    }
+}
