13048eb49a927cba27f0154bee935ca06d3996a9
simulan <simulan88@yahoo.com>
2017-12-10 23:34:25 +0100

    TextureCubeData & Shaders

---
+++ UMLProgram/Core/Render/TexturedCube/Programs/VertexShader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLProgram.Core.Render.TexturedCube.Programs {
+    public class VertexShader {
+        public static readonly string Text = @"
+            #version 330 core
+            layout(location = 0) in vec3 vertexPosition_modelspace;
+            layout(location = 1) in vec2 vertexUV;
+            out vec2 UV;
+            uniform mat4 projection_matrix;
+            uniform mat4 view_matrix;
+            uniform mat4 model_matrix;
+
+            void main(){
+
+                // UV of the vertex. No special space for this one.
+                UV = vertexUV;
+            }
+        ";
+    }
+}
