414739db4a488c59956a7ee9efc5cd82834d7bdd
simulan <simulan88@yahoo.com>
2017-12-14 23:28:48 +0100

    blenderloader namespace

---
+++ UMLProgram/Core/Loaders/BlenderLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLProgram.Core.Loaders {
+    public class BlenderLoader {
+        public static bool Load(string filename) {
+            FileStream stream = new FileStream(filename, FileMode.Open);
+            ValidateBlenderFileStream(stream);
+            return true;
+        }
+        private static void ValidateBlenderFileStream(FileStream stream) {
+            if (stream.CanRead) {
+                
+            } else {
+                throw new IOException(stream.Name + " does not support reading atm.");
+            }
+        }
+    }
+}
