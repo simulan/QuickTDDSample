89c6b919b1cd47ad63c9c44aadddc13830d6bc14
simulan <simulan88@yahoo.com>
2017-12-30 14:03:10 +0100

    buffer normalmaps

--- UMLProgram/Core/Render/Common/ModelBuffer2.cs
+++ UMLProgram/Core/Render/Common/ModelBuffer2.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
 using UMLProgram.Core.Loaders.Files;
+using OpenTK;
 
 namespace UMLProgram.Core.Render.Common {
     public class ModelBuffer2 {
@@ -12,16 +14,18 @@ namespace UMLProgram.Core.Render.Common {
             public int vertex;
             public int uv;
             public int normal;
+            public int tan;
+            public int bitan;
         }
-        public Tuple<IndexedD3Model, BufferHandle> this[int i] { get { return models[i]; } }
+        public Tuple<IndexedD3Model2, BufferHandle> this[int i] { get { return models[i]; } }
 
-        public int Add(IndexedD3Model model) {
+        public int Add(IndexedD3Model2 model) {
             BufferHandle handle = Buffer(model);
             int key = handle.vertex;
-            models.Add(handle.vertex, Tuple.Create<IndexedD3Model, BufferHandle>(model, handle));
+            models.Add(handle.vertex, Tuple.Create<IndexedD3Model2, BufferHandle>(model, handle));
             return key;
         }
-        private BufferHandle Buffer(IndexedD3Model m) {
+        private BufferHandle Buffer(IndexedD3Model2 m) {
             BufferHandle handle = new BufferHandle();
             if (m.Vertices != null && m.Vertices.Length != 0) {
                 GL.GenBuffers(1, out handle.vertex);
@@ -37,6 +41,16 @@ namespace UMLProgram.Core.Render.Common {
                 GL.GenBuffers(1, out handle.normal);
                 GL.BindBuffer(BufferTarget.ArrayBuffer, handle.normal);
                 GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, new IntPtr(m.Normals.Length * Vector3.SizeInBytes), m.Normals, BufferUsageHint.StaticDraw);
+            }
+            if (m.Tan != null && m.Tan.Length != 0) {
+                GL.GenBuffers(1, out handle.tan);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, handle.tan);
+                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, new IntPtr(m.Tan.Length * Vector3.SizeInBytes), m.Tan, BufferUsageHint.StaticDraw);
+            }
+            if (m.Bitan != null && m.Bitan.Length != 0) {
+                GL.GenBuffers(1, out handle.bitan);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, handle.bitan);
+                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, new IntPtr(m.Bitan.Length * Vector3.SizeInBytes), m.Bitan, BufferUsageHint.StaticDraw);
             }
             return handle;
         }
